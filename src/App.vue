5a2c152b4f4ba359883b3ab0f8463f8276b24495
Lucas Alves <lucasalvesmnss@gmail.com>
2020-12-16 16:50:10 -0300

    Adicionado o código de barras

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-      <v-col>
-        <v-row>
-          <vue-qrious :value="text" size="300" />
-        </v-row>
-        <v-row>
-          <v-flex xs8 sm4 md4 lg3>
-            <v-text-field
-              placeholder="Digite Aqui !!"
-              solo
-              v-model="text"
-            ></v-text-field>
-          </v-flex>
-        </v-row>
-      </v-col>
+  <v-col>
+    <vue-qrious :value="text" size="300" />
+    <barcode :value="text" />
+    <v-flex xs8 sm4 md4 lg3>
+      <v-text-field
+        placeholder="Digite Aqui !!"
+        solo
+        v-model="text"
+      ></v-text-field>
+    </v-flex>
+  </v-col>
 </template>
 
-<script src="/path/to/qrious.js"></script> 
+<script scr src="/path/to/qrious.js"></script> 
 <script>
 import VueQrious from "vue-qrious";
 import Qrious from "qrious";
+import VueBarcode from "vue-barcode";
 export default {
   name: "App",
   components: {
     VueQrious,
+    barcode: VueBarcode,
   },
   data: () => ({
     text: "",
   }),
 };
 </script>
+
